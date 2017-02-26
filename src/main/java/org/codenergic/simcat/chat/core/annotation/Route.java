d2fd062ec70defbdb5a605b6b4d4fa0a395889f9
dianw <dianw.aditya@gmail.com>
2017-02-27 00:48:42 +0700

    Create HTTP router annotation for easier request mapping

---
+++ src/main/java/org/codenergic/simcat/chat/core/annotation/Route.java
@@ -0,0 +1,18 @@
+package org.codenergic.simcat.chat.core.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.vertx.core.http.HttpMethod;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Route {
+	String path();
+
+	HttpMethod[] method() default {};
+}
