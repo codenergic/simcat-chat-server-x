ddaa1ddf64e961edc87413c15e95fdfc943df431
dianw <dianw.aditya@gmail.com>
2017-04-20 00:12:02 +0700

    #7: Add SMS/email code verification endpoint

--- src/main/java/org/codenergic/simcat/auth/authentication/rest/PasswordlessLoginRestService.java
+++ src/main/java/org/codenergic/simcat/auth/authentication/rest/PasswordlessLoginRestService.java
@@ -3,10 +3,8 @@ package org.codenergic.simcat.auth.authentication.rest;
 import java.util.Arrays;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.container.Suspended;
@@ -15,7 +13,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.codenergic.simcat.auth.core.auth0.Auth0Service;
-import org.codenergic.simcat.auth.core.auth0.Auth0Service.Connection;
 import org.codenergic.simcat.chat.core.annotation.WebComponent;
 import org.codenergic.simcat.core.web.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,8 +26,24 @@ public class PasswordlessLoginRestService {
 	private Auth0Service auth0Service;
 
 	@POST
-	public void sendVerificationCode(PasswordlessLoginRestData data, @PathParam("conn") @DefaultValue("SMS") Connection conn,
-			@Suspended AsyncResponse response) {
+	@Path("/verify")
+	public void loginWithVerificationCode(PasswordlessLoginRestData data, @Suspended AsyncResponse response) {
+		Validator.isNotBlank(data.getCode(), "code property must be provided");
+		Validator.isNotBlank(data.getUsername(), "username property must be provided");
+		Validator.isNotNull(data.getConnection(), "connection property must be provided");
+		auth0Service.verifyCode(data.getUsername(), data.getCode(), data.getConnection(), h -> {
+			if (h.succeeded())
+				response.resume(h.result());
+			else
+				response.resume(Response
+						.status(Status.BAD_REQUEST)
+						.entity(Arrays.asList(h.cause().getMessage()))
+						.build());
+		});
+	}
+
+	@POST
+	public void sendVerificationCode(PasswordlessLoginRestData data, @Suspended AsyncResponse response) {
 		Validator.isNotBlank(data.getUsername(), "username property must be provided");
 		Validator.isNotNull(data.getConnection(), "connection property must be provided");
 		auth0Service.sendVerificationCode(data.getUsername(), data.getConnection(), h -> {
